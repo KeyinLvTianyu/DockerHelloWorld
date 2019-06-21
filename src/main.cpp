295831850eec07a60f9726bacdd5b10b37912bab
KeyinLvTianyu <50066977+KeyinLvTianyu@users.noreply.github.com>
2019-06-21 13:11:39 +0800

    update opencv

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,32 @@
 #include <iostream>
+#include <opencv2/opencv.hpp>
 #include "bird.h"
 using namespace std;
-
-int main()
+using namespace cv;
+int main(int argc, char** argv)
 {
     bird b;
     b.fly();
-    return 0;
+	if (argc != 2)
+	{
+		cout<<"usage: DisplayImage.out <Image_Path>"<<endl;
+		return -1;
+	}
+
+	Mat image;
+	image = imread(argv[1], 1);
+
+	if (!image.data)
+	{
+		cout << "No image data"<<endl;
+		return -1;
+	}
+	namedWindow("Display Image", WINDOW_AUTOSIZE);
+	imshow("Display Image", image);
+
+	waitKey(0);
+
+	return 0;
+
 }
+
